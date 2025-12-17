60c5ebc49192bde9e8ba6b9536fd42b809909fd4
Linfeng Zhao <zlf0625@gmail.com>
2025-12-16 18:35:25 -0800

    flow: export Pipeline in core.flow

--- retriever/core/flow/__init__.py
+++ retriever/core/flow/__init__.py
@@ -10,6 +10,7 @@ from retriever.core.flow.adapter import Adapter, Latest, Hold, Window, Events
 from retriever.core.flow.config import FlowConfig
 from retriever.core.flow.handle import FlowHandle
 from retriever.core.flow.context import FlowContext
+from retriever.core.flow.pipeline import Pipeline
 from retriever.core.flow.graph import FlowGraph, FlowNode, FlowEdge
 from retriever.core.flow.io import flow_io, is_flow_io
 from retriever.core.flow.service import handle_service, call_service
@@ -36,6 +37,7 @@ __all__ = [
     'FlowConfig',
     'FlowHandle',
     'FlowContext',
+    'Pipeline',
 
     # Graph
     'FlowGraph',
