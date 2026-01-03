079e43bce78a20f13a78f43b1435b42a93a01526
Linfeng Zhao <zlf0625@gmail.com>
2026-01-03 13:02:47 -0500

    refactor(backend): update channel implementations for new buffer API

--- src/retriever/rt/backend/multiprocessing/channel.py
+++ src/retriever/rt/backend/multiprocessing/channel.py
@@ -9,7 +9,8 @@ from multiprocessing.connection import Connection
 from queue import Empty
 from typing import Any, List, Optional
 
-from retriever.flow.adapter import Adapter, EventBuffer
+from retriever.flow.adapter import Adapter
+from retriever.flow.frp import EventBuffer
 from retriever.rt.buffer_engine import BufferEngineKind, create_buffer_engine
 
 
